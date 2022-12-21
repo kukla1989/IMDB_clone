c1a3ec5c34deda20ecf17f44b8e3ab2e1348d993
kukla1989 <kukla.1989@ukr.net>
2022-12-21 20:31:10 +0200

    add movie, rate, category model

---
+++ db/migrate/20221221154720_create_rates.rb
@@ -0,0 +1,11 @@
+class CreateRates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rates do |t|
+      t.integer :rating
+      t.integer :user_id
+      t.integer :movie_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_071800) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_182651) do
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "average_rating"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rates", force: :cascade do |t|
+    t.integer "rating"
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
